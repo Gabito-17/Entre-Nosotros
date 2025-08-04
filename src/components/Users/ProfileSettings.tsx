08482e7265fa596d1615142b045758865f9429e5
XGabito12 <juangabrielpavon@gmail.com>
2025-08-04 06:57:54 -0300

    correccion

--- src/components/Users/ProfileSettings.tsx
+++ src/components/Users/ProfileSettings.tsx
@@ -135,7 +135,7 @@ export default function ProfileSettings() {
           >
             <motion.div
               className="avatar"
-              variants={pulse}
+              variants={expandFade}
               animate="animate"
               initial="initial"
             >
